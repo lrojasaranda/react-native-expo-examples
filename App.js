b1d94a5aadcd3c9f8fddb968f0fd55abd557ff1e
Leonel Rojas <leonelrojas18@gmail.com>
2023-07-01 21:28:12 -0500

    TUTORIAL-NAVIGATION #1

--- App.js
+++ App.js
@@ -1,21 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, DrawerActions, getFocusedRouteNameFromRoute } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+import LoginScreen from './src/screens/login/Login';
+import ConfirmScreen from './src/screens/login/Confirm';
+import HomeScreen from './src/screens/home/Home';
+import AccountScreen from './src/screens/account/index';
+import TripScreen from './src/screens/trip/index';
+
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const RootStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Trip" component={TripScreen} />
+    </Tab.Navigator>
   );
-}
+};
+const Drawer = createDrawerNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeDrawer = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeTabs} />
+      <Drawer.Screen name="Account" component={AccountScreen} />
+
+    </Drawer.Navigator>
+  );
+};
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  const handleSignIn = () => {
+    // TODO implement real sign in mechanism
+
+    setIsAuthenticated(true);
+  };
+  const handleSignOut = () => {
+    // TODO implement real sign out mechanism
+
+    setIsAuthenticated(false);
+  };
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        {isAuthenticated ? (
+          <RootStack.Screen name="Home" component={HomeDrawer}
+            options={({ route, navigation }) => ({
+              headerTitle: getFocusedRouteNameFromRoute(route),
+              headerLeft: () => (
+                <Button
+                  onPress={() =>
+                    navigation.dispatch(DrawerActions.toggleDrawer())
+                  }
+                  title="Menu"
+                />
+              ),
+              headerRight: () => (
+                <Button onPress={handleSignOut} title="Sign Out" />
+              ),
+            })}
+          />
+        ) : (
+          <>
+            <RootStack.Screen name="Login" component={LoginScreen} options={{
+              animationTypeForReplace: 'pop',
+            }}
+            />
+            <RootStack.Screen name="Confirm">
+              {(props) => (
+                <ConfirmScreen {...props} onSignIn={handleSignIn} />
+              )}
+            </RootStack.Screen>
+          </>
+        )}
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # react-native-expo-examples
+
+## Navigation
+### Installation
+```bash
+npm install @react-navigation/native
+npm install react-native-gesture-handler
+npm install react-native-reanimated
+npm install react-native-screens
+npm install react-native-safe-area-context
+npm install @react-native-community/masked-view
+```
+
+```bash
+npm install @react-navigation/stack
+```
+### Usage
+
+```bash
+npm install @react-navigation/drawer
+```
+
+```bash
+npm install @react-navigation/bottom-tabs
+```
+
+```babel.config.js
+#agregar plugin reanimated
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
+    
+  };
+};
+```
